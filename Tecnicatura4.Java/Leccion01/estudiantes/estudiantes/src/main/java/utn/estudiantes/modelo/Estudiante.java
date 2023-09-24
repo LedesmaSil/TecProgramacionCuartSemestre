61004fe740b7b15c3f28b4f2f6ef64fea5d1ef40
Silvana S. Ledesma <113069949+LedesmaSil@users.noreply.github.com>
2023-09-24 01:17:47 -0300

    SEMANA 3 Java

---
+++ Tecnicatura4.Java/Leccion01/estudiantes/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
@@ -0,0 +1,29 @@
+package utn.estudiantes.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+//bolilerplate - Repetitivo
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEstudiente;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+
+
+}
