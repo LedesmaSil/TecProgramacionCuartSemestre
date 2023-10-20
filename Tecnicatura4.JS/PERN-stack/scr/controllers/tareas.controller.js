1d3350c57f5fd17b971ce958a430ef266ce91d92
Silvana S. Ledesma <113069949+LedesmaSil@users.noreply.github.com>
2023-10-20 12:42:57 -0300

    SEMANA 5 JavaScript
    PROYECTO PERN-STACK

---
+++ Tecnicatura4.JS/PERN-stack/scr/controllers/tareas.controller.js
@@ -0,0 +1,9 @@
+export const listarTareas = (req, res) => res.send('obteniendo tareas');
+
+export const listarTarea = (req, res) => res.send('obteniendo tareas unica');
+
+export const crearTarea = (req, res) => res.send('creando tareas');
+
+export const actualizarTarea = (req, res) => res.send('actualizando tareas unica');
+
+export const eliminarTarea = (req, res) => res.send('eliminando tareas unica');
